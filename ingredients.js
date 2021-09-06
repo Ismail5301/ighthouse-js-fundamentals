b6836759f00cdaa14baf5edf40125464ac4ea9f6
Abdurahman Ismail Abdi <ismail530@gmail.com>
2021-09-06 09:13:17 +0000

    ingredients for banana bread

---
+++ ingredients.js
@@ -0,0 +1,12 @@
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+while (ingredients < 0 ) {
+	console.log(ingredients);
+	}
+	for (const ingredient of ingredients){
+	console.log(ingredients)
+	}
+	for (let i = 0; i <1 ; i++)
+	while (ingredients < 8) {
+	console.log(ingredients.reverse)
+	}
+	
